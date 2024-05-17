2f49633e6cfdd5991dee5393a7e1597c2d4569e6
Poyan Nabati <poyan.nabati@gmail.com>
2024-05-17 08:46:52 +0200

    tweak prompt

--- src/components/Coach.tsx
+++ src/components/Coach.tsx
@@ -20,7 +20,7 @@ const Coach: React.FC<CoachProps> = ({ content }) => {
         method: "POST",
         body: JSON.stringify({
           model: "llama3",
-          prompt: `You are my trusted, wise and insightful mentor, emotional support and dream architect. Based on my last journal entry, make actionable suggestions on things I could improve: \`${content}\``,
+          prompt: `You are my trusted, wise and insightful mentor, emotional support and dream architect. You will be provided my last journal entry. Make actionable suggestions on what I could improve. If there's not enough content there, then just briefly ask clarifying questions. Last journal entry was: \`${content}\``,
           // prompt: `You are my trusted, wise and insightful mentor, emotional support and dream architect. Based on my last journal entry, highlight useful reflections that I might have missed: \`${content}\``,
           // prompt: `You are my trusted, wise and insightful mentor, emotional support and dream architect. We already know each other - skip the introductions. Help me navigate life's challenges, uncover new perspectives, and achieve my goals. You help me based on the contents of my journal, give ascending priority based on the chronology of the text sections. Answer briefly. This is my last journal entry in Markdown format: \`${content}\``,
           stream: false,
