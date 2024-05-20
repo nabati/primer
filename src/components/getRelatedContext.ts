af341fc24ed753984ab4236662c00fa8d6171f4e
Poyan Nabati <poyan.nabati@gmail.com>
2024-05-20 11:25:10 +0200

    increase match count

--- src/components/getRelatedContext.ts
+++ src/components/getRelatedContext.ts
@@ -5,13 +5,13 @@ const getRelatedContext = async (query: string): Promise<string[]> => {
   if (query === "") {
     return [];
   }
-  
+
   const embedding = await generateEmbeddings(query);
 
   const { data } = await getSupabaseClient().rpc("match_chunks", {
     query_embedding: embedding, // Pass the embedding you want to compare
     match_threshold: 0.5, // Choose an appropriate threshold for your data
-    match_count: 3, // Choose the number of matches
+    match_count: 5, // Choose the number of matches
   });
 
   if (data === null) {
