32dbdfd89b894a016796485298c88ca2b7dda633
Poyan Nabati <poyan.nabati@gmail.com>
2024-05-26 17:54:44 +0200

    decrease threshold

--- src/components/getRelatedContext.ts
+++ src/components/getRelatedContext.ts
@@ -12,7 +12,7 @@ const getRelatedContext = async (
   const embedding = await generateEmbeddings(query);
   const { data } = await getSupabaseClient().rpc("match_chunks", {
     query_embedding: embedding, // Pass the embedding you want to compare
-    match_threshold: 0.7, // Choose an appropriate threshold for your data
+    match_threshold: 0.6, // Choose an appropriate threshold for your data
     match_count: 5, // Choose the number of matches
     excluded_journal_id: excludedJournalId, // Exclude a specific journal ID
   });
