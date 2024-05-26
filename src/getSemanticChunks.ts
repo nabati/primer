82a0880fb43147ecee3f7c953d6042641c2fa666
Poyan Nabati <poyan.nabati@gmail.com>
2024-05-26 17:53:55 +0200

    add semantic chunking

---
+++ src/getSemanticChunks.ts
@@ -0,0 +1,45 @@
+import cosineSimilarity from "./components/cosineSimilarity.ts";
+import generateEmbeddings from "./components/generateEmbeddings.ts";
+import markdownSplitter from "./markdownSplitter.ts";
+
+const MERGING_SIMILARITY_THRESHOLD = 0.5;
+
+const getSemanticChunks = async (text: string): Promise<string[]> => {
+  const chunks = markdownSplitter(text);
+
+  const embeddings = await Promise.all(
+    chunks.map((chunk) => generateEmbeddings(chunk)),
+  );
+
+  const similarities = embeddings.map((embedding, index) => {
+    if (index === embeddings.length - 1) {
+      return 0;
+    }
+
+    return cosineSimilarity(embedding, embeddings[index + 1]);
+  });
+
+  // If two chunks are similar, merge them
+  const mergedChunks = chunks.reduce<string[]>((acc, chunk, index) => {
+    if (index === 0) {
+      acc.push(chunk);
+      return acc;
+    }
+
+    if (similarities[index - 1] > MERGING_SIMILARITY_THRESHOLD) {
+      const lastMergedChunk = acc[acc.length - 1] ?? "";
+      acc[acc.length - 1] = `${lastMergedChunk} ${chunk}`;
+      return acc;
+    }
+
+    acc.push(chunk);
+    return acc;
+    //
+    // const lastMergedChunk = acc[acc.length - 1] ?? "";
+    // acc[acc.length - 1] = `${lastMergedChunk} ${chunk}`;
+    // return acc;
+  }, []);
+  return mergedChunks;
+};
+
+export default getSemanticChunks;
