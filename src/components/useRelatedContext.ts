65b127073b4d7bae760fa889a24fbe1d2419b0fb
Poyan Nabati <poyan.nabati@gmail.com>
2024-05-27 20:23:52 +0200

    add ContextEntry type

--- src/components/useRelatedContext.ts
+++ src/components/useRelatedContext.ts
@@ -2,6 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import getRelatedContext from "./getRelatedContext.ts";
 import { Activity } from "./store.ts";
 
+export type ContextEntry = {
+  content: string[];
+  journal_created_at: string;
+  journal_updated_at: string;
+};
+
 const useRelatedContext = ({
   activity,
   journalId,
@@ -12,7 +18,7 @@ const useRelatedContext = ({
   const { data, isFetching } = useQuery({
     queryKey: ["related-context", journalId, activity.content],
     queryFn: async () => {
-      const context: string[] = await getRelatedContext(
+      const context: ContextEntry[] = await getRelatedContext(
         activity.content,
         journalId,
       );
