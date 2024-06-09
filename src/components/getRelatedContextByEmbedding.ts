72bee05e78ac69333aa1128dea0254f8ef94e622
Poyan Nabati <poyan.nabati@gmail.com>
2024-06-09 12:52:22 +0200

    define getRelatedContextByEmbedding that takes an embedding as the thing to match on

---
+++ src/components/getRelatedContextByEmbedding.ts
@@ -0,0 +1,23 @@
+import { getSupabaseClient } from "../supabaseClient.ts";
+import { ContextEntry } from "./useRelatedContext.ts";
+
+const getRelatedContextByEmbedding = async (
+  embedding: number[],
+  excludedJournalId: string | undefined,
+): Promise<ContextEntry[]> => {
+  const { data } = await getSupabaseClient().rpc("match_chunks", {
+    query_embedding: embedding, // Pass the embedding you want to compare
+    match_threshold: 0.6, // Choose an appropriate threshold for your data
+    match_count: 5, // Choose the number of matches
+    excluded_journal_id: excludedJournalId, // Exclude a specific journal ID
+  });
+
+  if (data === null) {
+    return [];
+  }
+
+  console.debug("@@related context", data);
+  return data;
+};
+
+export default getRelatedContextByEmbedding;
