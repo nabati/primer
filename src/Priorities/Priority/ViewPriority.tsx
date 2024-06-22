477bbd7b9d95f1d9b93d58a542098b9b90996d41
Poyan Nabati <poyan.nabati@gmail.com>
2024-06-22 14:32:28 +0200

    clean up

--- src/Priorities/Priority/ViewPriority.tsx
+++ src/Priorities/Priority/ViewPriority.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   TextField,
